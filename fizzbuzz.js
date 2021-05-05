3a6efcaf9643e44911f45ceebf81bc9997e8877e
mateo1423 <75327637+mateo1423@users.noreply.github.com>
2021-05-05 13:50:09 +0200

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,8 @@
+function fizzBuzz(num) {
+    let result = [];
+    if(num%3 == 0) result.push("Fizz");
+    if(num%5 == 0) result.push("Buzz");
+    return result.join("") || undefined;
+}
+
+module.exports = fizzBuzz;
